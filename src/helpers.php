d5bb5d5a9c7ec331f82df20ced3bbf825255cddc
Sebastien HEYD <sebastien.heyd@free.fr>
2022-03-14 19:18:50 +0100

    Laravel 9

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,7 @@
 if (! function_exists('packages_path')) {
     function packages_path($path = '')
     {
-        $pathPrefix = rtrim(Storage::disk('packages')->getAdapter()->getPathPrefix(), DIRECTORY_SEPARATOR);
+        $pathPrefix = rtrim(config('filesystems.disks.packages.root'), DIRECTORY_SEPARATOR);
 
         if (! empty($path)) {
             $pathPrefix .= DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR);
